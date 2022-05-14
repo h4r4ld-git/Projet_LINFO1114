672ca725819652c144b7f488306a240c87853fc7
NoahFraiture <94681915+NoahFraiture@users.noreply.github.com>
2022-05-14 12:18:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pageRankLinear
 import pageRankPower
+import CSVReader
 
 def main():
-    print("h")
+    matrice_adjacencies = CSVReader.reader_csv("adjacences.csv")
+    vector_personnalisation = CSVReader.reader_csv("VecteurPersonnalisation_Groupe30.csv")
+    teleportation_factor = 0.9
+
+    score_linear_method = pageRankLinear.pageRankLinear(matrice_adjacencies, teleportation_factor, vector_personnalisation)
+    score_power_method = pageRankPower.pageRankPower(matrice_adjacencies, teleportation_factor, vector_personnalisation)
 
 if __name__ == "__main__":
     main()
